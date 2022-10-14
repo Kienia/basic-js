b5e864659ffb611a52f7cff173742131295c4a68
Kanstantsin Kiyenia <kienya.space@gmail.com>
2022-10-14 13:02:17 +0300

    feat: add task solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+
+  return arr.reduce((result, item, idx, array) => {
+    if (item === '--discard-next' && item != array[array.length - 1]) {
+      array.splice(idx, 2)
+      return result;
+    }
+
+    if (item === '--discard-prev' && item != array[0]) {
+      result.pop();
+      return result;
+    }
+
+    if (item === '--double-next' && item != array[array.length - 1]) {
+      result.push(arr[idx + 1]);
+      return result;
+    }
+
+    if (item === '--double-prev' && item != array[0]) {
+      result.push(arr[idx - 1]);
+      return result;
+    }
+
+    if (item === '--double-next' || item === '--discard-prev' || item === '--double-prev' || item === '--discard-next') {
+      return result;
+    }
+
+    result.push(item);
+    return result;
+  }, [])
+
 }
 
 module.exports = {
